e3a212771aa5ba063061d304d3e90b2557e536bc
凯翔 闭 <1477530671@qq.com>
2023-04-06 16:43:01 +0800

    1

--- hard/hard/3.cpp
+++ hard/hard/3.cpp
@@ -11,18 +11,6 @@ using namespace std;
 int main()
 {
 
-	int a[10][10];
-	for (int i = 0; i < 9; i++)
-	{
-		a[9][i] = i;
-	}
-	//Êä³öa
-	for (int i = 0; i < 9; i++)
-	{
-		cout << a[9][i] << " ";
-	}
-
-
 	system("pause");
 	return 0;
 }
